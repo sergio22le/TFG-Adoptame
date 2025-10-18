9278766278a5cf46ff07b6f8b89f0c63def9a526
Sergio <sergiolopezelio@hotmail.com>
2025-10-18 17:06:22 +0200

    React + Tailwind y Laravel configurado

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="h-full w-auto m-auto p-20 text-center">
+      <h1 className="text-4xl font-bold text-verdeMarcaSecundario">TAILWIND FUNCIONANDO</h1>
+      <h2 className="text-2xl font-semibold text-verdeMarcaPrimario">Página principal de Adóptame</h2>
+    </div>
+      
+  );
+}
+
+export default App;
